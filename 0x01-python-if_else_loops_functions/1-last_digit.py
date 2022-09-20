5a9b4dee23a4462691c93fc524de90e6d5e464f3
PEDA-ALPHA1 <peda4sure@gmail.com>
2022-09-20 14:59:44 -0700

    Assign a random signed value to a variable each time executed

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -8,7 +8,7 @@ else:
 if (L_digit > 5):
     print(f"Last digit of {number:d} is {L_digit:d} and is greater than 5")
 elif (L_digit < 6) and (L_digit != 0):
-    print(f"Last digit of {number:d} is {L_digit:d}\
-            and is less than 6 and not 0")
+    print(f"Last digit of {number:d} is {L_digit:d}"\
+            f" and is less than 6 and not 0")
 else:
     print(f"Last digit of {number:d} is {L_digit:d} and is 0")
