d1f9cff18ee4f115fc5340cf012d9df8077472f3
PEDA-ALPHA1 <peda4sure@gmail.com>
2022-09-20 17:27:12 -0700

    Task 8,9,&10

---
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def print_last_digit(number):
+
+    if number >= 0:
+        lastd = number % 10
+    if number < 0:
+        lastd = (number % -10) * -1
+    print("{:d}".format(lastd), end='')
+    return lastd
