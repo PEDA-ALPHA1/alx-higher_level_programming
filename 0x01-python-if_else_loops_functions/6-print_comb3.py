9e2776c7c50e607ad4ef2ce35bebe6440616fb72
PEDA-ALPHA1 <peda4sure@gmail.com>
2022-09-20 17:01:41 -0700

    Print number combination

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+for num in range(0, 9):
+    for comb in range(num + 1, 10):
+        if num == 8:
+            print("{}{}".format(num, comb))
+        else:
+            print("{}{}".format(num, comb), end=", ")
+    
