5d4a3c47b69139bb449f3c11f9bedfd20ffe6f02
PEDA-ALPHA1 <peda4sure@gmail.com>
2022-09-24 06:24:04 -0700

    Import files for calculations

---
+++ 0x02-python-import_modules/1-calculation.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+import calculator_1
+cal = calculator_1
+a = 10
+b = 5
+print("{} + {} = {}".format(a, b, cal.add(a, b)))
+print("{} - {} = {}".format(a, b, cal.sub(a, b)))
+print("{} * {} = {}".format(a, b, cal.mul(a, b)))
+print("{} / {} = {}".format(a, b, cal.div(a, b)))
