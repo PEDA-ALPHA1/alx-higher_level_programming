3ea6e2f81e54985c1d02dbba27aad9adc9b0ce7f
PEDA-ALPHA1 <daep100@gmail.com>
2022-10-12 16:49:06 -0700

    Class with defined attributes

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,14 +1,25 @@
 #!/usr/bin/python3
-"""Area of a square"""
+"""Module of elaborated class Square"""
 
 
 class Square:
-    """Square defined class with private sttribute size"""
+    """Square defined class with private attribute size"""
+    def __init__(self, size=0):
+        if isinstance(size, int):
+            if size < 0:
+                raise ValueError("size must be >= 0")
+            else:
+                self.__size = size
+        elif not isinstance(size, int):
+            raise TypeError("size must be an integer")
+'''
+class Square:
     def __init__(self, size=0):
         if not isinstance(size, int):
-                raise TypeError("must be an int")
+            raise TypeError("must be an int")
         elif isinstance(size, int):
             if size < 0:
                 raise ValueError("size must be >= 0")
             else:
                 self.__size = size
+'''
