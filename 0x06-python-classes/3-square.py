545eb04c1b308a354ca382886c76c2f363f80cf2
PEDA-ALPHA1 <daep100@gmail.com>
2022-10-12 17:12:58 -0700

    Area of a square

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -2,17 +2,16 @@
 """Area of a square"""
 
 
-
 class Square:
     """Square defined class with private attributes"""
     def __init__(self, size=0):
         if isinstance(size, int):
             if size < 0
-            raise ValueError ("size must be >= 0")
+            raise ValueError("size must be >= 0")
             else:
                 self.__size = size
         elif not isinstance(size, int):
-            raise TypeError ("size must be an integer")
+            raise TypeError("size must be an integer")
 
     def area(self):
         """Return area of a square"""
