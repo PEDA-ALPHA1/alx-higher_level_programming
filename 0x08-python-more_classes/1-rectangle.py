f5cac1406428769744a04177918adb908ae01c13
PEDA-ALPHA1 <daep100@gmail.com>
2022-10-17 17:32:58 -0700

    Build a rectangle

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -1,33 +1,37 @@
 #!/usr/bin/python3
-"""Build a Rectangle"""
+"""Rectangle."""
 
 
-class Rectangle():
+class Rectangle:
+    """A rectangle class."""
+
     def __init__(self, width=0, height=0):
-        self.__width = width
-        self.__height = height
+        """Create a new rectangle."""
+        self.width = width
+        self.height = height
 
     @property
     def width(self):
-        """Method to get value of width"""
+        """Return the width of the rectangle."""
         return self.__width
 
     @width.setter
-    def width(self, value):
-        """method to set the value width"""
-        if type(value) != int:
+    def width(self, width):
+        if type(width) is not int:
             raise TypeError("width must be an integer")
-        if value < 0:
+        if width < 0:
             raise ValueError("width must be >= 0")
-        else:
-            self.__width = value
+        self.__width = width
+
+    @property
+    def height(self):
+        """Return the height of the rectangle."""
+        return self.__height
 
     @height.setter
-    def height(self, value):
-        """method to set the value of height"""
-        if type(value) != int:
+    def height(self, height):
+        if type(height) is not int:
             raise TypeError("height must be an integer")
-        if value < 0:
+        if height < 0:
             raise ValueError("height must be >= 0")
-        else:
-            self.__height = value
+        self.__height = height
