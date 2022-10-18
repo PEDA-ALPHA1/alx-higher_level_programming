fd30860267daed00cdafcf20d5919e7a9ffe4315
PEDA-ALPHA1 <daep100@gmail.com>
2022-10-17 17:34:35 -0700

    Build a rectangle

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -1,61 +1,59 @@
 #!/usr/bin/python3
-"""Module for str and repr"""
+"""Rectangle."""
 
 
 class Rectangle:
-    """class rectangle defined with private inst attrib width and height"""
+    """A rectangle class."""
+
     def __init__(self, width=0, height=0):
-        self.__width = width
-        self.__height = height
+        """Create a new rectangle."""
+        self.width = width
+        self.height = height
 
     @property
     def width(self):
-        """method to get value of width"""
+        """Return the width of the rectangle."""
         return self.__width
 
     @width.setter
-    def width(self, value):
-        """method to set the value of width"""
-        if type(value) != int:
+    def width(self, width):
+        if type(width) is not int:
             raise TypeError("width must be an integer")
-        if value < 0:
+        if width < 0:
             raise ValueError("width must be >= 0")
-        else:
-            self.__width = value
+        self.__width = width
 
     @property
     def height(self):
-        """method to get value of height"""
+        """Return the height of the rectangle."""
         return self.__height
 
     @height.setter
-    def height(self, value):
-        """method to set the value of height"""
-        if type(value) != int:
+    def height(self, height):
+        if type(height) is not int:
             raise TypeError("height must be an integer")
-        if value < 0:
+        if height < 0:
             raise ValueError("height must be >= 0")
-        else:
-            self.__height = value
+        self.__height = height
 
     def area(self):
-        """publ intance method to determine area of rect"""
-        return int(self.__width) * int(self.__height)
+        """Return the area of the rectangle."""
+        return self.width * self.height
 
     def perimeter(self):
-        """publ inst method to determine perimeter of rect"""
-        if int(self.__width) == 0 or int(self.__height) == 0:
+        """Return the perimeter of the rectangle."""
+        if self.width == 0 or self.height == 0:
             return 0
-        else:
-            return (int(self.__width) * 2) + (int(self.__height) * 2)
+        return 2 * (self.width + self.height)
 
     def __str__(self):
-        """built in to return printed representation of string instance"""
-        picture = ""
+        """Return the string equivalent of the rectangle."""
+        rect = ""
+        if self.width == 0 or self.height == 0:
+            return ""
         for i in range(self.height):
             for i in range(self.width):
-                if self.height == 0 or self.width == 0:
-                    return picture
-                picture += "#"
-            picture += '\n'
-        return picture[:-1]
+                rect += "#"
+            rect += "\n"
+        return rect[:-1]
+
