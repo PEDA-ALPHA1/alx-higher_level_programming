2b26acf0241662542f1e837410b3078958693f83
PEDA-ALPHA1 <daep100@gmail.com>
2022-12-20 23:07:18 +0000

    A rectangle class

--- 0x13-javascript_objects_scopes_closures/1-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -1,7 +1,8 @@
 #!/usr/bin/node
+// JS Script
 module.exports = class Rectangle {
-	constructor (w, h) {
-		this.width = w;
-		this.height = h;
-	}
+  constructor (w, h) {
+    this.width = w;
+    this.height = h;
+  }
 };
