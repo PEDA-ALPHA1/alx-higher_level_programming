9d87e75d61ab8c726713a52a472b0cb2feb20a17
PEDA-ALPHA1 <daep100@gmail.com>
2022-12-21 00:38:24 +0000

    Correction

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,9 +1,10 @@
 #!/usr/bin/node
 // JS Script
 exports.esrever = function (list) {
-  let res = [];
-  for (let i = list.length - 1; i >= 0; i--) {
-    res.push(list[i]);
+  const revList = [];
+
+  for (let i = list.length - 1; i >= 0; --i) {
+    revList.push(list[i]);
   }
-  return res;
+  return revList;
 };
